363a6c2cf5cf2cc2661089f848dec199fc1fb58f
Olga A <a11os@yandex.ru>
2018-10-14 17:21:59 +0500

    Добавлены CRUD методы для модели Product

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -10,21 +10,24 @@ urlpatterns = [
          name='user_update'),
     path('users/delete/<slug:title>/', adminapp.user_delete, \
          name='user_delete'),
-    path('catalog/create/', adminapp.category_create, \
+
+    path('catalog/create/', adminapp.CategoryCreate.as_view(), \
          name='category_create'),
     path('catalog/', adminapp.CategoryList.as_view(), name='category'),
     path('catalog/update/<slug:slug>/', adminapp.CategoryUpdate.as_view(), \
          name='category_update'),
-    path('products/', adminapp.ProductList.as_view(), name='product_list'),
     path('catalog/delete/<slug:title>/', adminapp.category_delete, \
          name='category_delete'),
-    path('products/create/category/<slug:title>/', adminapp.product_create, \
-         name='product_create'),
+
+    path('products/', adminapp.ProductList.as_view(), name='product_list'),
     path('products/category/<slug:title>/', adminapp.products, \
          name='products'),
-    path('products/<slug:title>/', adminapp.product_read, \
-         name='product_read'),
-    path('products/update/<slug:title>/', adminapp.product_update, \
+    # path('products/<slug:title>/', adminapp.product_read, \
+    #      name='product_detail'),
+
+    path('products/create/', adminapp.ProductCreate.as_view(), \
+         name='product_create'),
+    path('products/update/<slug:slug>/', adminapp.ProductUpdate.as_view(), \
          name='product_update'),
     path('products/delete/<slug:title>/', adminapp.product_delete, \
          name='product_delete'),
